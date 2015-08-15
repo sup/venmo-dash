e2593441038519ab13590a0373045643cef9fea2
Shikkic <Shikkic@gmail.com>
2015-08-15 12:55:36 -0400

    added user model

---
+++ app/models/user.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: String,
+    username: {
+        type: String,
+        unique: true
+    },
+    balance: String,
+    provider: String,
+    salt: String,
+    venmo: {},
+    access_token: String,
+    refresh_token: String 
+});
+
+module.exports = mongoose.model('User', userSchema);
