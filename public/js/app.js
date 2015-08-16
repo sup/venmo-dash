41caa0809fd0aea9938a475be7cb756fc5367cf0
Charles Lai <cjl223@cornell.edu>
2015-08-15 19:23:10 -0700

    add progress bar and removing modal

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
         tagName: 'div',
 
-        template: '<div class="col l4 m6 s12"><div class="card-panel"><div class="row content"><div class="col s3 avatar"><img src="{{profile_picture_url}}" alt="" class="circle responsive-img"></div><div class="col s9 info"><div class="username">{{display_name}}</div><div class="ratio">ratio!</div><a class="modal-trigger waves-effect waves-light  btn light-blue lighten-3" href="#modal1">Details</a></div></div></div></div><div id="modal1" class="modal"><div class="modal-content"><h4>Username</h4><p>Info about the user</p><div class="row"><div class="col s4 graph">Graphs?</div><div class="col s4 graph">Graphs?</div><div class="col s4 graph">Graphs?</div></div></div><div class="modal-footer"><a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Exit</a><a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Pay</a><a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Charge</a></div></div>',
+        template: '<div class="col l4 m6 s12"><div class="card-panel"><div class="row content"><div class="col s3 avatar"><img src="{{profile_picture_url}}" alt="" class="circle responsive-img"></div><div class="col s9 info"><div class="username">{{display_name}}</div><div class="progress"> <div class="progress-bar progress-bar-success" style="width: 90%"> <span class="sr-only">$90</span> </div> <div class="progress-bar progress-bar-danger" style="width: 10%"> <span class="sr-only">$10</span> </div> </div><a class="modal-trigger waves-effect waves-light  btn light-blue lighten-3" href="#modal1">Details</a></div></div></div></div>',
 
         initialize: function() {
             this.render();
